d04a480d03bf63ac3aae3eff2535cbcfb751d65b
Gustavo Sozzi <bomgustavo10@gmail.com>
2025-06-10 12:59:43 -0300

    corrigido funcionalidades

--- calculadoraBinario.py
+++ calculadoraBinario.py
@@ -45,34 +45,14 @@ def twoComplement(binary):
     return vai_um
 
 
-def binarySubstraction(number_one, number_two):
-    def binarySubstraction(number_one, number_two):
-        if len(number_one) == 0: return
-        if len(number_two) == 0: return
+def binarySubtraction(number_one, number_two):
+    number_one, number_two = normalizarStrings(number_one, number_two)
+    n = len(number_one)
+    b_invertido = ''.join('1' if bit == '0' else '0' for bit in number_two)
 
-        number_one, number_two = normalizarStrings(number_one, number_two)
-        IdxStart = 0
-        EndIndex = len(number_one) - 1
-        carry = [0] * len(number_one)
-        result = ''
-
-        while EndIndex >= IdxStart:
-            x = int(number_one[EndIndex])
-            y = int(number_two[EndIndex])
-            sub = (carry[EndIndex] + x) - y
-
-            if sub == -1:
-                result += '1'
-                carry[EndIndex] = -1
-            elif sub == 1:
-                result += '1'
-            elif sub == 0:
-                result += '0'
-            else:
-                raise Exception('Error')
-
-            EndIndex -= 1
-        return result[::-1]
+    b_complemento = binary_sum(b_invertido, '0' * (n - 1) + '1')
+    resultado = binary_sum(number_one, b_complemento)
+    return resultado[-n:]
 
 def normalizarStrings(number_one, number_two):
     diff = abs((len(number_one) - len(number_two)))
@@ -114,7 +94,7 @@ def main():
             print(f'Resultado da soma de: {number_one_binary} : ({number_one})  + , {number_two_binary} : ({number_two})  = ', result)
 
         elif(operador == '-'):
-            total = binarySubstraction(number_one_binary, number_two_binary)
+            total = binarySubtraction(number_one_binary, number_two_binary)
             result = convert_to_decimal(total)
             print(f'Resultado da subtração de: {number_one_binary} : ({number_one})  + , {number_two_binary} : ({number_two})  = ',total ,result)
 
